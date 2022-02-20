a33d7ea54e407d0c0c67914b7c59a98ecba57525
KaishAli <360mohdkaishali.cs@gmail.com>
2022-02-20 17:58:26 +0530

    Updates

---
+++ precache-manifest.9905dca446fc932e00f8d6dd174fa22c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fbc8a50ffeed72beef4",
+    "url": "/tic-tac-toe-game-with-react/static/css/main.bd054d68.chunk.css"
+  },
+  {
+    "revision": "5fbc8a50ffeed72beef4",
+    "url": "/tic-tac-toe-game-with-react/static/js/main.5fbc8a50.chunk.js"
+  },
+  {
+    "revision": "f741bdb9e5c16289b43e",
+    "url": "/tic-tac-toe-game-with-react/static/js/1.f741bdb9.chunk.js"
+  },
+  {
+    "revision": "a5da5393eb2cb6d2f888",
+    "url": "/tic-tac-toe-game-with-react/static/js/runtime~main.a5da5393.js"
+  },
+  {
+    "revision": "e1716c6eb8a753c0ed09701facec6f2c",
+    "url": "/tic-tac-toe-game-with-react/index.html"
+  }
+];
